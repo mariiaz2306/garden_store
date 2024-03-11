b3f366345add1da005253f90d1148bda441c8503
daria kutluieva <daryakutlueva@gmail.com>
2024-03-11 11:32:17 +0100

    Added header and routing for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import './style/app.scss';
-import Footer from "./components/Footer/Footer"
-import MainPage from './pages/mainPage/MainPage';
+import "./style/app.scss";
+import { Outlet } from "react-router-dom";
+import Footer from "./Layout/Footer/Footer";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import { ThemeProvider } from "./contexts/ThemeProvider";
+import Header from "./Layout/Header/Header";
 
 function App() {
   return (
-   <>
-
-<Footer/>
-   <MainPage/>
-   </>
+    <Provider store={store}>
+      <ThemeProvider>
+        <div className="App">
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
---
+++ src/components/DiscountButton/DiscountButton.jsx
@@ -0,0 +1,13 @@
+// DiscountButton.js
+import React from "react";
+import "./DiscountButton.scss";
+
+const DiscountButton = ({ onClick }) => {
+  return (
+    <button className="discount-button" onClick={onClick}>
+      1 day discount!
+    </button>
+  );
+};
+
+export default DiscountButton;
