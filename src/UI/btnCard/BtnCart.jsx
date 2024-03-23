0979a6f964db31d4c2ecbac06960cb2a81517424
your_username <mariianader2305@gmail.com>
2024-03-23 20:29:00 +0100

    the cartOrder part has been done, as well as CartModalWindow & CartPage

--- src/UI/btnCard/BtnCart.jsx
+++ src/UI/btnCard/BtnCart.jsx
@@ -5,50 +5,46 @@ import { Link } from "react-router-dom";
 
 
 
+
 export const ButtonTypes = {
     ADD_TO_CART: "ADD_TO_CART",
-   
-};
 
-export default function BtnCart({type}){
-    const [isSubmitting, setIsSubmitting] = useState(false);
+  };
+  
+  export default function BtnCard({ type }) {
+    const [isSubmitting, setSubmitting] = useState(false);
+  
+    const handleClick = () => {
+      if (type === ButtonTypes.ADD_TO_CART) {
+        setSubmitting(true);
+        setTimeout(() => {
+          setSubmitting(false);
+        }, 2000);
+      } 
+    };
+  
 
-    const handleClick = ()=>{
-        if(type === ButtonTypes.ADD_TO_CART){
-            setIsSubmitting(true);
-            setTimeout(()=>setIsSubmitting(false), 2000);
-        }
-    }
-
-
+  
     const buttonClass = classNames(s.card_btn, {
-        [s.add_btn]: type === ButtonTypes.ADD_TO_CART,
-        [s.submitted]: isSubmitting,
-        [s.btn_green]:
-          type === ButtonTypes.ADD_TO_CART  ||
-          type === ButtonTypes.GO_HOME
+      [s.add_btn]: type === ButtonTypes.ADD_TO_CART,
+      [s.submitted]: isSubmitting,
+      [s.btn_green]:
+        type === ButtonTypes.CHECK_OUT ||
+        type === ButtonTypes.GO_HOME ||
+        type === ButtonTypes.SHOPPING,
     });
-
+  
     const addBtnClass = classNames(s.add_btn, {
-        [s.disabled_hover]: isSubmitting && type === ButtonTypes.ADD_TO_CART,
+      [s.disabled_hover]: isSubmitting && type === ButtonTypes.ADD_TO_CART,
     });
-
-    return(
-        <div className={buttonClass}>
-            {type === ButtonTypes.ADD_TO_CART && (
-                <button onClick = {handleClick} className={addBtnClass}>
-                    {isSubmitting ? "Added" : "Add to cart"}
-                </button>
-            )}
-
-
-            {type === ButtonTypes.GO_HOME && (
-               <Link to ={"/"}>
-                   <button className={s.btn_green}>Go Home</button>
-               </Link>
-            )}
-        </div>
-    )
-
-    
-}
+  
+    return (
+      <div className={buttonClass}>
+        {type === ButtonTypes.ADD_TO_CART && (
+          <button onClick={handleClick} className={addBtnClass}>
+            {isSubmitting ? "Added" : "Add to cart"}
+          </button>
+        )}
+          </div>
+  );
+}  
---
+++ src/utils/cartLocalStorage.js
@@ -0,0 +1,8 @@
+export const loadCartItems = () => {
+    const savedCart = localStorage.getItem('cart');
+    return  savedCart ? JSON.parse(savedCart) : [];
+}
+
+export const saveCartItems = (cart) => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
