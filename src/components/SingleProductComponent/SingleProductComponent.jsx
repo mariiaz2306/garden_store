070ea5b475806e8e6974879c26f3285946d28bc7
Stan Lincoln <stanizbash@gmai.com>
2024-04-08 12:44:49 +0200

    new changes

--- src/components/SingleProductComponent/SingleProductComponent.jsx
+++ src/components/SingleProductComponent/SingleProductComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 
@@ -9,6 +9,7 @@ import { addLikedProduct, removeLikedProduct } from '../../store/slices/likedPro
 import s from './style.module.scss' // Импорт стилей модуля
 import heart from '../../media/icons/heart.svg' // Импорт иконки сердца
 import heartWhite from '../../media/icons/heartWhite.svg'
+import greenHeart from '../../media/icons/greenHeart.svg'
 import { addProduct } from '../../store/slices/cartSlice.js';
 
 // Компонент для модального окна
@@ -35,6 +36,7 @@ export default function SingleProductComponent() {
 
     dispatch(addProduct({ ...product, quantity: 1 })) // Предполагаем, что el - это объект товара с нужными полями
   }
+
   // Состояния для управления модальным окном и количеством товаров
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [count, setCount] = useState(0)
@@ -48,17 +50,23 @@ export default function SingleProductComponent() {
   // Функция для переключения обрезанного описания
   const toggleTruncate = () => setIsTruncated(!isTruncated)
 
-
-  const likedProducts = useSelector((state) => state.likedProducts.likedProducts)
-  const isLiked = likedProducts.some((likedProduct) => likedProduct?.id === product?.id)
+  const [isHeartClicked, setIsHeartClicked] = useState(() => {
+    const heartState = localStorage.getItem(`isHeartClicked_${id}`);
+    return heartState ? JSON.parse(heartState) : false;
+  });
 
   const toggleLiked = () => {
-    if (isLiked) {
-      dispatch(removeLikedProduct(product))
+    setIsHeartClicked(!isHeartClicked);
+    if (!isHeartClicked) {
+      dispatch(addLikedProduct(product));
     } else {
-      dispatch(addLikedProduct(product))
+      dispatch(removeLikedProduct(product));
     }
-  }
+  };
+
+  useEffect(() => {
+    localStorage.setItem(`isHeartClicked_${id}`, JSON.stringify(isHeartClicked));
+  }, [id, isHeartClicked]);
 
   // Вывод индикатора загрузки при загрузке данных о продукте
   if (isLoading) return <p>Loading...</p>
@@ -95,8 +103,8 @@ export default function SingleProductComponent() {
         {/* Блок с заголовком и иконкой "Добавить в избранное" */}
         <div className={s.headerContainer}>
           <p className={s.header}>{product.title}</p>
-          <button className={s.icon_button} onClick={toggleLiked} isLiked={isLiked}>
-            <img src={theme === 'light' ? heart : heartWhite} alt="Add to favorites" />
+          <button className={s.icon_button} onClick={toggleLiked}>
+            <img src={isHeartClicked ? greenHeart : (theme === 'light' ? heart : heartWhite)} alt="Add to favorites" />
           </button>
         </div>
         {/* Блок с ценой и скидкой */}
--- src/components/homeComponents/productComponent/productsItem/ProductsItem.jsx
+++ src/components/homeComponents/productComponent/productsItem/ProductsItem.jsx
@@ -1,47 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Price from '../../../../UI/price/Price'
-import { BASE_URL } from '../../../../config'
-import { useDispatch } from 'react-redux'
-import { useSelector } from 'react-redux'
-import { addLikedProduct, removeLikedProduct } from '../../../../store/slices/likedProductsSlice'
-import { addProduct, deleteProduct } from '../../../../store/slices/cartSlice'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { addLikedProduct, removeLikedProduct } from '../../../../store/slices/likedProductsSlice';
+import { addProduct, deleteProduct } from '../../../../store/slices/cartSlice';
+import { BASE_URL } from '../../../../config';
+import Price from '../../../../UI/price/Price';
+import BtnCart, { ButtonTypes } from '../../../../UI/btnCard/BtnCart';
 
-import s from './ProductsItem.module.css'
-import shoppingBag1 from '../../../../media/icons/shoppingBag1.svg'
-import heart from '../../../../media/icons/heart.svg'
-import BtnCart, { ButtonTypes } from '../../../../UI/btnCard/BtnCart'
+import s from './ProductsItem.module.css';
+import heart from '../../../../media/icons/heart.svg';
+import blackHeart from '../../../../media/icons/blackHeart.svg';
+import greenHeart from '../../../../media/icons/greenHeart.svg';
+import shoppingBag1 from '../../../../media/icons/shoppingBag1.svg';
+import blackBag from '../../../../media/icons/blackBag.svg';
+import greenBag from '../../../../media/icons/greenBag.svg';
 
 export default function ProductItem({ el }) {
-  const dispatch = useDispatch()
-  const { theme } = useSelector((state) => state.theme)
-  const handleAddToCart = (e) => {
-    e.preventDefault()
+  const dispatch = useDispatch();
+  const [isHeartClicked, setIsHeartClicked] = useState(() => {
+    const heartState = localStorage.getItem(`isHeartClicked_${el.id}`);
+    return heartState ? JSON.parse(heartState) : false;
+  });
 
-    dispatch(addProduct({ ...el, quantity: 1 })) // Предполагаем, что el - это объект товара с нужными полями
-  }
+  const [isBagClicked, setIsBagClicked] = useState(() => {
+    const bagState = localStorage.getItem(`isBagClicked_${el.id}`);
+    return bagState ? JSON.parse(bagState) : false;
+  });
 
-  const cartProducts = useSelector((state) => state.cart.products)
-  const isProductInCart = cartProducts.some((product) => product.id === el.id)
+  const cartProducts = useSelector((state) => state.cart.products);
+  const isProductInCart = cartProducts.some((product) => product.id === el.id);
+  const likedProducts = useSelector((state) => state.likedProducts.likedProducts);
+  const isLiked = likedProducts.some((likedProduct) => likedProduct.id === el.id);
+
+  const handleToggleLiked = () => {
+    const newState = !isHeartClicked;
+    setIsHeartClicked(newState);
+    if (!isLiked) {
+      dispatch(addLikedProduct(el));
+    } else {
+      dispatch(removeLikedProduct(el));
+    }
+    localStorage.setItem(`isHeartClicked_${el.id}`, JSON.stringify(newState));
+  };
 
   const handleToggleCart = () => {
-    if (isProductInCart) {
-      dispatch(deleteProduct(el.id)) // Удаляем товар из корзины
+    const newState = !isBagClicked;
+    setIsBagClicked(newState);
+    if (!isProductInCart) {
+      dispatch(addProduct({ ...el, quantity: 1 }));
     } else {
-      dispatch(addProduct({ ...el, quantity: 1 })) // Добавляем товар в корзину
+      dispatch(deleteProduct(el.id));
     }
-  }
+    localStorage.setItem(`isBagClicked_${el.id}`, JSON.stringify(newState));
+  };
 
-  const likedProducts = useSelector((state) => state.likedProducts.likedProducts)
-  const isLiked = likedProducts.some((likedProduct) => likedProduct.id === el.id)
-
-  const toggleLiked = () => {
-    if (isLiked) {
-      dispatch(removeLikedProduct(el))
-    } else {
-      dispatch(addLikedProduct(el))
-    }
-  }
+  const handleBtnCartClick = () => {
+    setIsBagClicked(true); // Устанавливаем состояние isBagClicked в true при нажатии на кнопку BtnCart
+    dispatch(addProduct({ ...el, quantity: 1 }));
+    localStorage.setItem(`isBagClicked_${el.id}`, JSON.stringify(true));
+  };
 
   return (
     <div className={s.products_wrapper}>
@@ -50,33 +67,29 @@ export default function ProductItem({ el }) {
           <img src={`${BASE_URL}${el.image}`} alt={el.title} className={s.products_img} />
         </Link>
         <div className={s.icons_wrapper}>
-          {/* Кнопка добавления в избранное */}
-          <button className={s.icon_button} onClick={toggleLiked} isLiked={isLiked}>
-            <img className={s.svgIcon} src={heart} alt="Add to favorites" />
+          <button className={s.icon_button} onClick={handleToggleLiked}>
+            <img src={isHeartClicked ? greenHeart : heart} alt="Add to favorites" />
           </button>
-
           <button className={s.icon_button} onClick={handleToggleCart}>
-            <img src={shoppingBag1} alt="Add to cart" />
+            <img src={isBagClicked ? greenBag : shoppingBag1} alt="Add to cart" />
           </button>
         </div>
       </div>
-
-      <div className={s.add_btn} onClick={handleAddToCart}>
-        <BtnCart type={ButtonTypes.ADD_TO_CART} onClick={handleAddToCart} />
+      <div className={s.add_btn} onClick={handleBtnCartClick}>
+        <BtnCart type={ButtonTypes.ADD_TO_CART} onClick={handleBtnCartClick} />
       </div>
-
       <div className={s.products_information}>
         <Link to={`/products/${el.id}`} className={s.products_link}>
           <h3 className={s.products_title}>{el.title}</h3>
         </Link>
         <Price
           el={el}
-          theme={theme}
           realPriceClass={s.real_price}
           oldPriceClass={s.old_price}
           saleValueClass={s.sale_value}
         />
       </div>
     </div>
-  )
+  );
 }
+
