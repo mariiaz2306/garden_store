6d173c3c81cd8b74bed9faf141277093a6efca23
your_username <mariianader2305@gmail.com>
2024-04-16 14:56:01 +0200

    the styles fixing

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ export default function Banner() {
     <div className="banner">
       <div className="banner__content">
         <h1 className="banner__content__header">Amazing Discounts on Garden Products!</h1>
-        <Link to="/sales">
+        <Link to="/sales" className='banner__content__link'>
           <button className="banner__content__checkOutBtn">Check out</button>
         </Link>
       </div>
